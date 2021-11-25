eef96f9bb6a37c1115f2c61072878ed4b6bc4cab
jacopo1996 <jacopo1996@live.it>
2021-11-25 11:30:15 +0100

    Update commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 /** carouselwords-script **/
 document.addEventListener("DOMContentLoaded", function() {
-  var words = ["HTML", "CSS", "Bootstrap", "Sass", "Proattività", "People Oriented", "Italiano", "Inglese"];
-  var counter = 0;
+  let words = ["HTML", "CSS", "Bootstrap", "Sass", "Proattività", "People Oriented", "Italiano", "Inglese"];
+  let counter = 1;
   setInterval(update_carousel_words, 1500);
   function update_carousel_words(){
     document.getElementById("carousel").innerHTML = words[counter];
