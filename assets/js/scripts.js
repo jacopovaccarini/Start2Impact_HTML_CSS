316a3974cb629718260caaacbdcc0390aac40c50
jacopo1996 <jacopo1996@live.it>
2022-07-19 10:18:45 +0200

    Update Projects

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,26 @@
+/** caroselloORcards-script **/
+document.addEventListener("DOMContentLoaded", caroselloType);
+window.addEventListener('resize', caroselloType);
+
+function caroselloType() {
+  let windowWidth = window.innerWidth;
+
+  let carosello = document.getElementById("carouselExampleIndicators");
+  let carosello_small = document.getElementById("cardSmallScreen");
+
+  if (windowWidth <= 767) {
+    carosello.setAttribute("hidden","");
+    carosello_small.removeAttribute("hidden");
+  } else {
+    carosello.removeAttribute("hidden");
+    carosello_small.setAttribute("hidden","");
+  }
+}
+
+
 /** carouselwords-script **/
 document.addEventListener("DOMContentLoaded", function() {
-  let words = ["HTML", "CSS", "JavaScript", "Bootstrap", "Sass", "webpack", "Axios", "Proattività", "People Oriented", "Precisione", "Creatività", "Italiano", "Inglese"];
+  let words = ["HTML", "CSS", "JavaScript", "Bootstrap", "Sass", "webpack", "Axios", "Vue.js", "Proattività", "People Oriented", "Precisione", "Creatività", "Italiano", "Inglese"];
   let counter = 1;
   setInterval(update_carousel_words, 1500);
   function update_carousel_words(){
